c53e3bdf46592e73cfeddec513e27a8608c9c510
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2021-09-07 17:45:08 +0200

    Add asynchronous mode

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-let getTime = () => {
-    const localDate = new Date();
+let getTime = async () => {
+    const localDate = await new Date();
     const utcOffset = localDate.getTimezoneOffset();
     const cetOffset = utcOffset + 60;
     const cestOffset = utcOffset + 120;
     const cetOffsetInMilliseconds = cetOffset * 60 * 1000;
     const cestOffsetInMilliseconds = cestOffset * 60 * 1000;
-    const cestDateStart = new Date();
-    const cestDateFinish = new Date();
-    const localDateTime = localDate.getTime();
+    const cestDateStart = await new Date();
+    const cestDateFinish = await new Date();
+    const localDateTime = await localDate.getTime();
     let cestDateStartTime;
     let cestDateFinishTime;
     cestDateStart.setTime(Date.parse('29 March ' + localDate.getFullYear() + ' 02:00:00 GMT+0100'));
     cestDateFinish.setTime(Date.parse('25 October ' + localDate.getFullYear() + ' 03:00:00 GMT+0200'));
-    cestDateStartTime = cestDateStart.getTime();
-    cestDateFinishTime = cestDateFinish.getTime();
-    let hResult = (localDateTime >= cestDateStartTime && localDateTime <= cestDateFinishTime) ? new Date(localDateTime + cestOffsetInMilliseconds) : new Date(localDateTime + cetOffsetInMilliseconds);
+    cestDateStartTime = await cestDateStart.getTime();
+    cestDateFinishTime = await cestDateFinish.getTime();
+    let hResult = (localDateTime >= cestDateStartTime && localDateTime <= cestDateFinishTime) ? await new Date(localDateTime + cestOffsetInMilliseconds) : await new Date(localDateTime + cetOffsetInMilliseconds);
     const hours = hResult.getHours() < 10 ? "0" + hResult.getHours() : hResult.getHours();
     const minutes = hResult.getMinutes() < 10 ? "0" + hResult.getMinutes() : hResult.getMinutes();
     const seconds = hResult.getSeconds() < 10 ? "0" + hResult.getSeconds() : hResult.getSeconds();
